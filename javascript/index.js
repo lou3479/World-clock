cb366573fdaa3f04c1134ad0bdb2320c0c5a6a8c
Louise Miller <louisevictoriamiller34@gmail.com>
2025-02-02 15:43:06 +0000

    Added 3rd city to homepage

--- javascript/index.js
+++ javascript/index.js
@@ -1,30 +1,42 @@
 setInterval(function (){
-//Los Angeles
-let losAngelesElement = document.querySelector("#los-angeles");
-if (losAngelesElement) {
-let losAngelesDateElement = document.querySelector ("#los-angeles .date");
-let losAngelesTimeElement = document.querySelector ("#los-angeles .time");
-let losAngelesTime = moment().tz("America/Los_Angeles");
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = document.querySelector("#los-angeles .date");
+    let losAngelesTimeElement = document.querySelector("#los-angeles .time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-);
-}
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  if (parisElement) {
+    let parisDateElement = document.querySelector("#paris .date");
+    let parisTimeElement = document.querySelector("#paris .time");
+    let parisTime = moment().tz("Europe/Paris");
 
-//Paris
-let parisElement = document.querySelector("#paris");
-if (parisElement) {
-let parisDateElement = document.querySelector("#paris .date");
-let parisTimeElement = document.querySelector("#paris .time");
-let parisTime = moment().tz("Europe/Paris");
+    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-parisTimeElement.innerHTML = parisTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
-}
+  //Cairo
+  let cairoElement = document.querySelector("#cairo");
+  if (cairoElement) {
+    let cairoDateElement = document.querySelector("#cairo .date");
+    let cairoTimeElement = document.querySelector("#cairo .time");
+    let cairoTime = moment().tz("Africa/Cairo");
+
+    cairoDateElement.innerHTML = cairoTime.format("MMMM Do YYYY");
+    cairoTimeElement.innerHTML = cairoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 },1000);
 
 function updateCity(event) {
@@ -45,7 +57,7 @@ function updateCity(event) {
           "h:mm:ss [<small>]A[</small>]"
         )}</div>
     </div>
-    <a href="/">All cities</a>
+    <a href="index.html">All cities</a>
     `;
 }
 
